fe68efa22a7e5f57afa05859d55e1370c2dd9995
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-11-07 10:23:19 +0100

    Either

--- examples/02_maybe.cpp
+++ examples/02_maybe.cpp
@@ -8,24 +8,24 @@ int main() {
         auto foo = Maybe<int>{true, 42};
 
         std::cout << "auto foo = Maybe<int>{true, 42};" << '\n';
-        std::cout << "foo.has_value = " << foo.has_value << '\n';
-        std::cout << "foo.value = " << foo.value << '\n';
+        std::cout << "foo.has_value == " << foo.has_value << '\n';
+        std::cout << "foo.value == " << foo.value << '\n';
         std::cout << '\n';
     }
     {
         auto foo = Maybe<int>{};
 
         std::cout << "auto foo = Maybe<int>{};" << '\n';
-        std::cout << "foo.has_value = " << foo.has_value << '\n';
-        std::cout << "foo.value = " << foo.value << '\n';
+        std::cout << "foo.has_value == " << foo.has_value << '\n';
+        std::cout << "foo.value == " << foo.value << '\n';
         std::cout << '\n';
     }
     {
         auto foo = Maybe<float>{};
 
         std::cout << "auto foo = Maybe<float>{};" << '\n';
-        std::cout << "foo.has_value = " << foo.has_value << '\n';
-        std::cout << "foo.value = " << foo.value << '\n';
+        std::cout << "foo.has_value == " << foo.has_value << '\n';
+        std::cout << "foo.value == " << foo.value << '\n';
         std::cout << '\n';
     }
     {
@@ -34,8 +34,8 @@ int main() {
 
         std::cout << "auto foo = Maybe<int>{true, 42};" << '\n';
         std::cout << "auto bar = fmap([](int a){return a+1;}, foo);" << '\n';
-        std::cout << "bar.has_value = " << bar.has_value << '\n';
-        std::cout << "bar.value = " << bar.value << '\n';
+        std::cout << "bar.has_value == " << bar.has_value << '\n';
+        std::cout << "bar.value == " << bar.value << '\n';
         std::cout << '\n';
         
         assert(43 == bar.value);
